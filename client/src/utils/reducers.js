aa5016d6d593589ea21074efc2da5c81c593b8d6
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-28 22:02:14 -0500

    updated reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,5 +1,3 @@
-import { useReducer } from 'react';
-
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -10,9 +8,17 @@ import {
   UPDATE_CART_QUANTITY,
   CLEAR_CART,
   TOGGLE_CART
-} from "./actions";
+} from "./actions"; 
 
-export const reducer = (state, action) => {
+const initialState = {
+  product: [],
+  categories: [],
+  currentCategory: [],
+  cart: [],
+  cartOpen: false
+};
+
+export const reducers = (state = initialState, action) => {
   switch (action.type) {
     // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
     case UPDATE_PRODUCTS:
@@ -82,6 +88,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducers;
