b5cf175e686b70d186ca894a0d259f7a2d84e76e
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-29 15:54:34 -0500

    install missing depend

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,9 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { StoreProvider } from './utils/GlobalState';
+// import { setContext } from '@apollo/client/link/context';
+import store from './utils/store';
+import { Provider } from 'react-redux';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -42,7 +43,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +54,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
-import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
